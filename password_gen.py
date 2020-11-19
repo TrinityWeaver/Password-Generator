17b4d0c7b0c6ea5477c7923a4dfbbfde509c6591
Sebastian Marynicz <62943472+TrinityWeaver@users.noreply.github.com>
2020-11-19 19:37:41 +0000

    beta_version_2.01

--- password_gen.py
+++ password_gen.py
@@ -15,81 +15,85 @@ print('Password Generator v.2.0\nAuthor: Sebastian Marynicz')
 firstChoice = input(
     'Would you like to create randomized password or hashed?\n Type in "Random" or "Hash": ')
 
-if firstChoice == "Random":
-    while True:
-    passwordLenght = int(input('How long your password needs to be?: '))
-    symbols = passwordLenght
-    passReqUpper = int(
-        input(f'How many Uppercase letters? (You can use {symbols} symbols): '))
-    symbols = passwordLenght - passReqUpper
-    passReqLower = int(
-        input(f'How many Lowercase letters? (You can use {symbols} symbols): '))
-    symbols = passwordLenght - passReqUpper - passReqLower
-    passReqPunkt = int(
-        input(f'How many Punctuation letters? (You can use {symbols} symbols): '))
-    symbols = passwordLenght - passReqUpper - passReqLower - passReqPunkt
-    passReqDigit = int(
-        input(f'How many Digits letters? (You can use {symbols} symbols): '))
-    passReqLenght = passReqUpper + passReqLower + passReqPunkt + passReqDigit
-    if passReqLenght > passwordLenght:
-        print('Password cannot be created as you put to many symbols in lenght of the password')
-        continue
+
+while firstChoice == "Random":
+    print('Default password created by Generator will be 10 symbols lenght,\nwith 2 uppercase,\nwith 2 lowercase,\nwith 2 symbols\nwith 2 digits.')
+    defaultPassword = input(
+        'Would you like to proceed with default or customized option?\nType in "Default" or "Custom": ')
+    if defaultPassword == 'Custom':
+        passwordLenght = int(input('How long your password needs to be?: '))
+        symbols = passwordLenght
+        passReqUpper = int(
+            input(f'How many Uppercase letters? (You can use {symbols} symbols): '))
+        symbols = passwordLenght - passReqUpper
+        passReqLower = int(
+            input(f'How many Lowercase letters? (You can use {symbols} symbols): '))
+        symbols = passwordLenght - passReqUpper - passReqLower
+        passReqPunkt = int(
+            input(f'How many Punctuation letters? (You can use {symbols} symbols): '))
+        symbols = passwordLenght - passReqUpper - passReqLower - passReqPunkt
+        passReqDigit = int(
+            input(f'How many Digits letters? (You can use {symbols} symbols): '))
+        passReqLenght = passReqUpper + passReqLower + passReqPunkt + passReqDigit
+        if passReqLenght > passwordLenght:
+            print(
+                'Password cannot be created as you put to many symbols in lenght of the password')
+            continue
+        else:
+            loopsforpass = True
+            break
+
     else:
+        loopsforpass = True
+        passReqUpper = 2
+        passReqLower = 2
+        passReqPunkt = 2
+        passReqDigit = 2
+        passwordLenght = 10
+        passReqLenght = 8
         break
 
 # Lists for creating and randomizing Password
-    password = []
-    ranUpper = []
-    ranLower = []
-    ranPunkt = []
-    ranDigit = []
-
-
-for upper in range(passReqUpper):
-    upper = random.choice(string.ascii_uppercase)
-    ranUpper.append(upper)
-
-for lower in range(passReqLower):
-    lower = random.choice(string.ascii_lowercase)
-    ranLower.append(lower)
-
-for punkt in range(passReqPunkt):
-    punkt = random.choice(string.punctuation)
-    ranPunkt.append(punkt)
-
-for digit in range(passReqDigit):
-    digit = random.choice(string.digits)
-    ranDigit.append(digit)
-
-
 # Lenght of leftovers symbols for future randomazing
-
-theLenghtofPassword = passwordLenght - passReqLenght
-
-
 # If user havent used all "Symbols", random the rest
-
-listOfstringMethods = string.ascii_letters + string.punctuation + string.digits
-
-
-for passwordRandom in range(theLenghtofPassword):
-    passwordRandom = random.choice(listOfstringMethods)
-    password.append(passwordRandom)
-
-# Randomize symbols, letters and digits
-
-newPassword = password + ranUpper + ranLower + ranPunkt + ranDigit
-
-random.shuffle(newPassword)
-
+# Randomize symbols, letters and digit
 # Print password
+password = []
+ranUpper = []
+ranLower = []
+ranPunkt = []
+ranDigit = []
+if loopsforpass:
+    for upper in range(passReqUpper):
+        upper = random.choice(string.ascii_uppercase)
+        ranUpper.append(upper)
+    for lower in range(passReqLower):
+        lower = random.choice(string.ascii_lowercase)
+        ranLower.append(lower)
+    for punkt in range(passReqPunkt):
+        punkt = random.choice(string.punctuation)
+        ranPunkt.append(punkt)
+    for digit in range(passReqDigit):
+        digit = random.choice(string.digits)
+        ranDigit.append(digit)
 
-print('Your secure password is: ', *newPassword, sep='')
+    theLenghtofPassword = passwordLenght - passReqLenght
+    listOfstringMethods = string.ascii_letters + string.punctuation + string.digits
 
-hashingQuest = input(
-    'Would you like to hash your password?\nAnswer with Yes or No: ')
-passBytes = bytes(f'{newPassword}', 'utf-8')
-print(passBytes)
-if hashingQuest == 'Yes':
-    hashedPass = bcrypt.hashpw(passBytes, bcrypt.gensalt())
-    print(hashedPass)
+    for passwordRandom in range(theLenghtofPassword):
+        passwordRandom = random.choice(listOfstringMethods)
+        password.append(passwordRandom)
+
+    newPassword = password + ranUpper + ranLower + ranPunkt + ranDigit
+    random.shuffle(newPassword)
+
+    print('Your secure password is: ', *newPassword, sep='')
+
+
+# Hashing Password
+# else:
+#passwordToHash = input('Provide a password for hashing: ')
+#passBytes = bytes(f'{passwordToHash}', 'utf-8')
+# print(passBytes)
+#hashedPass = bcrypt.hashpw(passBytes, bcrypt.gensalt(15))
+# print(hashedPass)
